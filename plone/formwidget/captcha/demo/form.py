465467d2dc90f8a9647caab7be70722866f54d4a
Timo Stollenwerk <timo@zmag.de>
2009-08-10 14:25:10 +0000

    captcha validation is working now.
    svn path=/plone.formwidget.captcha/trunk/; revision=28638

--- plone/formwidget/captcha/demo/form.py
+++ plone/formwidget/captcha/demo/form.py
@@ -1,8 +1,11 @@
+from Acquisition import aq_inner
 from zope import interface
-from z3c.form import form, field, button
 from zope import schema
+from zope.component import getMultiAdapter
+from z3c.form import form, field, button
 from plone.z3cform.layout import wrap_form
 from plone.formwidget.captcha.widget import CaptchaFieldWidget
+
 
 class ICaptchaForm(interface.Interface):
     captcha = schema.TextLine(title=u"Captcha",
@@ -22,6 +25,13 @@ class BaseForm(form.Form):
     @button.buttonAndHandler(u'Save')
     def handleApply(self, action):
         data, errors = self.extractData()
+        if data.has_key('captcha'):
+            # Verify the user input against the captcha
+            captcha = getMultiAdapter((aq_inner(self.context), self.request), name='captcha')
+            if captcha.verify(data['captcha']):
+                print 'Captcha validation passed.'
+            else:
+                print 'The code you entered was wrong, please enter the new one.'
         return
 
 CaptchaForm = wrap_form(BaseForm)
