7b84594b31027e5c54efb7eacce77f76ffce78c1
Timo Stollenwerk <timo@zmag.de>
2009-08-11 11:27:08 +0000

    allow "from plone.formwidget.captcha import CaptchaFieldWidget" instead of "from plone.formwidget.captcha.widget import CaptchaFieldWidget"
    svn path=/plone.formwidget.captcha/trunk/; revision=28680

--- plone/formwidget/captcha/__init__.py
+++ plone/formwidget/captcha/__init__.py
@@ -2,3 +2,4 @@ from zope.i18nmessageid import MessageFactory
 MessageFactory = MessageFactory('plone.formwidget.captcha')
 
 from plone.formwidget.captcha.widget import CaptchaWidget
+from plone.formwidget.captcha.widget import CaptchaFieldWidget
