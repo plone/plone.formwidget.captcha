9924b72520b8c6c56936f0888af1c9a9389a31dd
Timo Stollenwerk <timo@zmag.de>
2009-08-13 10:10:32 +0000

    allow "from plone.formwidget.captcha import CaptchaValidator".
    svn path=/plone.formwidget.captcha/trunk/; revision=28756

--- plone/formwidget/captcha/__init__.py
+++ plone/formwidget/captcha/__init__.py
@@ -3,3 +3,4 @@ CaptchaMessageFactory = MessageFactory('plone.formwidget.captcha')
 
 from plone.formwidget.captcha.widget import CaptchaWidget
 from plone.formwidget.captcha.widget import CaptchaFieldWidget
+from plone.formwidget.captcha.validator import CaptchaValidator
