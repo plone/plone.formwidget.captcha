99f7ab2b9f136f87eaa4fda0303db39c5b1709bb
Timo Stollenwerk <timo@zmag.de>
2009-08-28 15:58:47 +0000

    README and setup prepared for first release.
    svn path=/plone.formwidget.captcha/trunk/; revision=29453

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ version = '1.0a1'
 
 setup(name='plone.formwidget.captcha',
       version=version,
-      description="",
+      description="Captcha widget for Plone.",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
