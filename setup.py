23e4397e51e445bfebb69c98242fa4400c6f1a0b
Hanno Schlichting <hanno@hannosch.eu>
2010-01-27 15:04:36 +0000

    Updated package metadata and marked this as a Plone add-on.
    svn path=/plone.formwidget.captcha/trunk/; revision=33494

--- setup.py
+++ setup.py
@@ -1,23 +1,20 @@
 from setuptools import setup, find_packages
-import os
 
-version = '1.0a1'
+version = '1.0a2'
 
 setup(name='plone.formwidget.captcha',
       version=version,
       description="Captcha widget for Plone.",
       long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+                       open("CHANGES.txt").read(),
       classifiers=[
         "Framework :: Plone",
         "Programming Language :: Python",
-        "Topic :: Software Development :: Libraries :: Python Modules",
         ],
       keywords='',
       author='Timo Stollenwerk',
       author_email='timo@zmag.de',
-      url='http://svn.plone.org/svn/plone/plone.formwidget.captcha',
+      url='http://pypi.python.org/pypi/plone.formwidget.captcha',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['plone'],
@@ -25,12 +22,12 @@ setup(name='plone.formwidget.captcha',
       zip_safe=False,
       install_requires=[
           'setuptools',
-          # -*- Extra requirements: -*-
           'skimpyGimpy',
           'plone.keyring',
           'plone.z3cform',
       ],
       entry_points="""
-      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
       """,
       )
