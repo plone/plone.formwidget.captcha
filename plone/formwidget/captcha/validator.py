c1a4ac4b19e054e25450601a5c1ecc8a077a55b4
Timo Stollenwerk <timo@zmag.de>
2010-07-13 09:13:16 +0000

    whitespace
    svn path=/plone.formwidget.captcha/trunk/; revision=37758

--- plone/formwidget/captcha/validator.py
+++ plone/formwidget/captcha/validator.py
@@ -10,14 +10,17 @@ from zope.schema import ValidationError
 
 from plone.formwidget.captcha import CaptchaMessageFactory as _
 
+
 class WrongCaptchaCode(ValidationError):
     __doc__ = _("""The code you entered was wrong, please enter the new one.""")
+
 
 class CaptchaValidator(validator.SimpleFieldValidator):
 
     def validate(self, value):
         super(CaptchaValidator, self).validate(value)
-        captcha = getMultiAdapter((aq_inner(self.context), self.request), name='captcha')
+        captcha = getMultiAdapter((aq_inner(self.context), self.request), 
+                                  name='captcha')
         if value:
             if not captcha.verify(value):
                 raise WrongCaptchaCode
--- plone/formwidget/captcha/widget.py
+++ plone/formwidget/captcha/widget.py
@@ -9,6 +9,7 @@ from z3c.form import converter
 from z3c.form.browser import text
 from interfaces import ICaptchaWidget
 
+
 class CaptchaWidget(text.TextWidget):
     maxlength = 7
     size = 8
@@ -16,11 +17,15 @@ class CaptchaWidget(text.TextWidget):
     zope.interface.implementsOnly(ICaptchaWidget)
 
     def captchaImage(self):
-        self.captcha = zope.component.getMultiAdapter((aq_inner(self.context), self.request), name='captcha')
+        self.captcha = zope.component.getMultiAdapter((aq_inner(self.context), 
+                                                       self.request), 
+                                                       name='captcha')
         return self.captcha.image_tag()
 
     def captchaAudio(self):
-        self.captcha = zope.component.getMultiAdapter((aq_inner(self.context), self.request), name='captcha')
+        self.captcha = zope.component.getMultiAdapter((aq_inner(self.context), 
+                                                       self.request), 
+                                                       name='captcha')
         return self.captcha.audio_url()
 
 def CaptchaFieldWidget(field, request):
